43bad8e9e4a471bba20530afe09532e2068c6af1
Дом <@DESKTOP-7GF1H2I>
2017-08-20 17:57:58 +0500

    ввод изначальных чисел

--- задание6/Program.cs
+++ задание6/Program.cs
@@ -10,6 +10,66 @@ namespace задание6
     {
         static void Main(string[] args)
         {
+            double a,b,c;
+            int N, k;
+            Vvod("введите a",out a);            
+            Vvod("введите b", out b);           
+            Vvod("введите c", out c);           
+            VvodTwo("введите N", out N);
+            Proverka("число N", ref N);
+            VvodTwo("введите k", out k);
+            Proverka("число k", ref k);
+
+            double[] mas = new double[k];
+
+        }
+        static double function(double[] masiv,int x)
+        {
+            return masiv[x-1];
+        }
+        static double Vvod(string s, out double n)//проверка ввода
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = double.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
+        }
+     
+        static double VvodTwo(string s, out int n)//проверка ввода
+        {
+            bool ok;
+            string buf;
+            do
+            {
+                Console.Write(s + " = ");
+                buf = Console.ReadLine();
+                ok = Int32.TryParse(buf, out n);
+                if (!ok) Console.WriteLine("Введите " + s + " заново");
+            } while (!ok);
+            return n;
+        }
+        static void Proverka(string s, ref int a)
+        {
+            bool ok = false;
+            string buf;
+            do
+            {
+                if (a > 0) ok = true;
+                else
+                {
+                    if (!ok) Console.WriteLine("\aВведите " + s + " заново");
+                    Console.Write(s + " = ");
+                    buf = Console.ReadLine();
+                    ok = Int32.TryParse(buf, out a);
+                    ok = false;
+                }
+            } while (!ok);
         }
     }
 }
